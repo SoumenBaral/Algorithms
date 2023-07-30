f5a46465ff571b3e9bc54b6142af41095152300c
SoumenBaral <shumen.sb@gmail.com>
2023-07-30 21:40:59 +0600

    leaen about some time Complexity ;

---
+++ 1.Introduction/2.Asymptotic Notation and Time complexity/1.time_complexity.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int n;
+    cin>>n;
+    for(int i = 0; i<n;i++)
+    {
+        // time complexity is O(n);
+    }
+    for(int i = 0; i<n;i++)
+        {
+            for(int j = 0; j<n; j++)
+            {
+                // time complexity is O(n^2);
+            }
+        }
+
+        for(int i = 0; i<n;i*=2)
+    {
+        // time complexity is O(logN);
+    }
+
+    for(int i = 0; i<n;i++)
+    {
+        for(int i = 0; i<n;i*=2)
+        {
+            // time complexity is O(NlogN);
+        }
+
+    }
+    int p =0;
+    for(int i = 0; p<n;i++)
+    {
+      p+=i;
+      //complexity is O(^n)Root N;
+
+    }
+
+    // O(1)----Constant time complexity
+    //O(logN)--logarithmic complexity
+    //O(^n)---square Root complexity
+    // O(n)-----lennar complexity
+    // O(nlogN)
+    //O(2^n)--Quadric complexity 
+    return 0;
+}
