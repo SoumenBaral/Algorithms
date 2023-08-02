5c32c5bd65c48b7d5dd99f17c18dd071fd4472f8
SoumenBaral <shumen.sb@gmail.com>
2023-08-03 00:28:43 +0600

    Find Duplicate in an array

--- 1.Introduction/5.Practice/2.FindDuplicatreInanArray.cpp
+++ 1.Introduction/5.Practice/2.FindDuplicatreInanArray.cpp
@@ -1,49 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
-class Solution {
-public:
-    void marge(int l, int r, int mid,vector<int>& ar){
-    int left_size = mid-l+1;
-    int L[left_size+1];
-    int right_size = r-mid;
-    int R[right_size+1];
-    for(int i=l, j = 0 ;i<=mid; j++ ,i++){
-        L[j] =ar[i];
-    }
-    for(int i=mid+1, j = 0 ;i<=r; j++ ,i++){
-        R[j] =ar[i];
-    };
-     L[left_size] = INT_MAX;
-     R[right_size] = INT_MAX;
-     int lp=0;
-     int rp = 0;
-     for(int i= l;i<=r;i++){
-        if(L[lp]<=R[rp]){
-            ar[i]=L[lp];
-            lp++;
-        }
-        else{
-            ar[i]=R[rp];
-            rp++;
-        }
-     }
+#include <bits/stdc++.h>
 
+int findDuplicate(vector<int> &arr, int n){
+	sort(arr.begin(),arr.end());
+	int a;
+	for(int i = 0; i<n-1; i++){
+		if(arr[i]==arr[i+1])a=arr[i];
+	}
+	return a;
 }
-    void margeSort(int l, int r,vector<int>& ar){
-    if(l==r)return;
-    int mid = (l+r)/2;
-    margeSort(l,mid,ar);
-    margeSort(mid+1,r,ar);
-    marge(l,r,mid,ar);
-}
-    vector<int> sortArray(vector<int>& ar) {
-        int l = 0;
-        int r = ar.size()-1;
-        margeSort( l, r ,ar);
-        
-        return ar;
-    }
-};
+
 int main ()
 {
     
