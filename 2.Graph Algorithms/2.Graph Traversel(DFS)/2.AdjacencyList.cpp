73508fb569daa21ebb5a336b966f50f38bedaec2
SoumenBaral <shumen.sb@gmail.com>
2023-08-06 23:46:04 +0600

    adjacency list take input is done

--- 2.Graph Algorithms/2.Graph Traversel(DFS)/2.AdjacencyList.cpp
+++ 2.Graph Algorithms/2.Graph Traversel(DFS)/2.AdjacencyList.cpp
@@ -1,7 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
+const int N = 1e5+5;
+vector<int>adj[N];
 int main ()
 {
+    int n,m;
+    cin>>n>>m;
+    for(int i=0;i<m;i++){
+        int u,v,w;
+        cin>>u>>v;//W for Weighted Graph
+        adj[u].push_back(v); //Its for Directed Graph
+        adj[v].push_back(u); //Its for Undirected Graph
+
+    }
+  
     
     return 0;
 }
