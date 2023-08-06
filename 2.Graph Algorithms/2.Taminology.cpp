3aead484579d4e931247d3c5fd27c32856691f6e
SoumenBaral <shumen.sb@gmail.com>
2023-08-06 14:03:48 +0600

    Terminoloy is running

--- 2.Graph Algorithms/2.Taminology.cpp
+++ 2.Graph Algorithms/2.Taminology.cpp
@@ -1,3 +1,22 @@
 //We denote node via some kind of information
 // Node -----> Vertex
 // Connection -------->Edge
+
+//depending on Direction Graph is Two type
+//1.Directed 
+//2.UnDirected
+
+//Depending on the Edge (weight)
+// 1.weighted
+// 2.UnWeighted
+ 
+                    
+                    //        50
+                    //    A---------->B
+                    //    |\
+                    // 30 | \ 60
+                    //    |  \
+                    //    |   \
+                    //   \/   \/
+                    //   C------>D
+                    //      10
