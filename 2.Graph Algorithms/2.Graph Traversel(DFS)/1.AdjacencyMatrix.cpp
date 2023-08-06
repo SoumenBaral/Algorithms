f4fb455301032208b2617dc5f24aef5dc9a4a4f7
SoumenBaral <shumen.sb@gmail.com>
2023-08-06 21:29:24 +0600

    adjanceney Matrix of directed graph

--- 2.Graph Algorithms/2.Graph Traversel(DFS)/1.AdjacencyMatrix.cpp
+++ 2.Graph Algorithms/2.Graph Traversel(DFS)/1.AdjacencyMatrix.cpp
@@ -1,7 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
+const int N = 1e3+5;
+int adjmat[N][N];
 int main ()
 {
+    int n,m;
+    cin>>n>>m;
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        adjmat[u][v]=1;
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cout<<adjmat[i][j]<<" ";
+        }
+        cout<<endl;
+    }
     
     return 0;
 }
