3a4b0ea0d28a114bc0009994cdad5f80a1ccdda9
SoumenBaral <shumen.sb@gmail.com>
2023-08-06 20:45:53 +0600

    Basic Graph and Graph Representation is done

---
+++ 2.Graph Algorithms/1.Basic Graph and Graph Representation/4.GraphRepresentation.cpp
@@ -0,0 +1,30 @@
+// Take Input from Undirected Graph
+// 7 (n)---->Number of node
+// 12 (m)--->Number of Edges
+// 1  3
+// 2  4
+// 5  6   connection
+// 7  1 
+// 8  3
+// ----
+
+// Directed and Undirected Graphs taking input is same the difference is take the
+// Direction to 1st connection to second Element...
+
+
+// Weighted Graph
+// 7 (n)---->Number of node
+// 12 (m)--->Number of Edges
+// u  v  w
+// 1  3  40
+// 2  4  30
+// 5  6  20   connection
+// 7  1  50
+// 8  3  10
+// -------
+
+
+// Type of Representation
+// 1.Adjacency Matrix
+// 2.Adjacency  list
+// 3. List of Edges done
