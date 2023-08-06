771706a8267f30eca982925f4f9273b62e71c741
SoumenBaral <shumen.sb@gmail.com>
2023-08-06 14:48:54 +0600

    Representation done

--- 2.Graph Algorithms/GraphRepresentation.cpp
+++ 2.Graph Algorithms/GraphRepresentation.cpp
@@ -7,3 +7,18 @@
 // 7  1 
 // 8  3
 // ----
+
+// Directed and Undirected Graphs taking input is same the difference is take the
+// Direction to 1st connection to second Element...
+
+
+// Weighted Graph
+// 7 (n)---->Number of node
+// 12 (m)--->Number of Edges
+// u  v  w
+// 1  3  40
+// 2  4  30
+// 5  6  20   connection
+// 7  1  50
+// 8  3  10
+// -------
