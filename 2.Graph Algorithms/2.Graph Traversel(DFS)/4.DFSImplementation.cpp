e67020da5a917aec7d3d3c1491d370249a47d23b
SoumenBaral <shumen.sb@gmail.com>
2023-08-07 17:24:28 +0600

    DFS Basic

--- 2.Graph Algorithms/2.Graph Traversel(DFS)/4.DFSImplementation.cpp
+++ 2.Graph Algorithms/2.Graph Traversel(DFS)/4.DFSImplementation.cpp
@@ -2,14 +2,21 @@
 using namespace std;
 const int N = 1e5+5;
 vector<int>adj[N];
+bool visited[N];
 void dfs(int u){
     //Section 1
     //Action just after entering the node u
+    cout<<"visited Node : "<<  u<<endl;
+    visited[u]=true;
     for(int v: adj[u])
     {
         //Section 2
         //Action Before Entering Neighbor
-        dfs(v);
+        if(visited[u]==true)continue;
+        // if(visited[u] == false){
+              dfs(v);
+        // }
+      
         //Section 3
         //Action After Exiting the Neighbor
 
@@ -28,15 +35,19 @@ int main ()
         // adj[v].push_back(u); //Its for Undirected Graph
 
     }
-    for(int i=1;i<=n;i++){
-        cout<<"list "<<i<<" : ";
-        for(int j:adj[i]){
-            cout<<j<<" ";
-        }
-        cout<<endl;
+    // for(int i=1;i<=n;i++){
+    //     cout<<"list "<<i<<" : ";
+    //     for(int j:adj[i]){
+    //         cout<<j<<" ";
+    //     }
+    //     cout<<endl;
 
-    }
+    // }
   dfs(1);
+//   cout<<"Visiting array"<<":"<<endl;
+//   for(int i=1; i<=n;i++){
+//     cout<<"Node :"<<i<<" Status: "<<visited[i]<<endl;
+//   }
     
     return 0;
 }
--- 2.Graph Algorithms/2.Graph Traversel(DFS)/tempCodeRunnerFile.cpp
+++ 2.Graph Algorithms/2.Graph Traversel(DFS)/tempCodeRunnerFile.cpp
@@ -1,2 +1,4 @@
-
-    
+ cout<<"Visiting array"<<":"<<endl;
+//   for(int i=1; i<=n;i++){
+//     cout<<"Node :"<<i<<" Status: "<<visited[i]<<endl;
+//   }
