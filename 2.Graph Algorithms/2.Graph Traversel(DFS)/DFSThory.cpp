7d082bd71144ccc5694e2f52bb8442437fbe7f6e
SoumenBaral <shumen.sb@gmail.com>
2023-08-07 16:28:05 +0600

    DFS Thory is done

--- 2.Graph Algorithms/2.Graph Traversel(DFS)/DFSThory.cpp
+++ 2.Graph Algorithms/2.Graph Traversel(DFS)/DFSThory.cpp
@@ -1,6 +1,6 @@
 // DFS---->Depth First Search
 // BFS---->Breadth First Search
-DFS -------->jai rastay jabo tar joto govir a 
-            jaoa jay jaboo ai concept ta sobgula 
-            Node ar joinnoi hobe 
+// DFS -------->jai rastay jabo tar joto govir a 
+//             jaoa jay jaboo ai concept ta sobgula 
+//             Node ar joinnoi hobe 
 
