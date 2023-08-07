999347b5d9ac02a553b7fec3c4867d87d99e11a9
SoumenBaral <shumen.sb@gmail.com>
2023-08-07 16:58:51 +0600

    DFS Basic

--- 2.Graph Algorithms/2.Graph Traversel(DFS)/4.DFSImplementation.cpp
+++ 2.Graph Algorithms/2.Graph Traversel(DFS)/4.DFSImplementation.cpp
@@ -1,1 +1,42 @@
-cpp
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 1e5+5;
+vector<int>adj[N];
+void dfs(int u){
+    //Section 1
+    //Action just after entering the node u
+    for(int v: adj[u])
+    {
+        //Section 2
+        //Action Before Entering Neighbor
+        dfs(v);
+        //Section 3
+        //Action After Exiting the Neighbor
+
+    }
+    //Section 4
+    //Action Before Exiting Node u
+}
+int main ()
+{
+    int n,m;
+    cin>>n>>m;
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;//W for Weighted Graph
+        adj[u].push_back(v); //Its for Directed Graph
+        // adj[v].push_back(u); //Its for Undirected Graph
+
+    }
+    for(int i=1;i<=n;i++){
+        cout<<"list "<<i<<" : ";
+        for(int j:adj[i]){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+
+    }
+  dfs(1);
+    
+    return 0;
+}
