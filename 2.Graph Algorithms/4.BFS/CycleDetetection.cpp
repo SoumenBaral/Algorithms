97462daad7843e68b1c267d5b33226e6f5ba72b0
SoumenBaral <shumen.sb@gmail.com>
2023-08-11 03:30:43 +0600

    Cycle detected is done

---
+++ 2.Graph Algorithms/4.BFS/CycleDetetection.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 1e5+5;
+vector<int>adj[N];
+bool visited[N];
+bool dfs(int u ,int p = -1){
+    bool isCycleExist=false;
+    visited[u]= true;
+    for(int v: adj[u]){
+        if(v==p)continue;
+        if(visited[v])return true;
+       isCycleExist = isCycleExist | dfs(v,u);
+    }
+    return isCycleExist;
+}
+
+int main ()
+{
+    int n,m;
+    cin>>n>>m;
+    for(int i=0;i<m;i++)
+    {
+        int u,v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+  bool isCycle=false;
+  for(int i = 1; i<n;i++){
+    if(visited[i])continue;
+    isCycle = isCycle | dfs(i);
+  }
+
+   if(isCycle){
+    cout<<"Cycle Detected"<<endl;
+   }
+   else{
+    cout<<"No Cycle Detected"<<endl;
+   }
+}
