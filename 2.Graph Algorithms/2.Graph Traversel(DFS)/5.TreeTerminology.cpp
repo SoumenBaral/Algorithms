8547e412d0b5f1f0e87d3761256650d54b95ec54
SoumenBaral <shumen.sb@gmail.com>
2023-08-10 12:25:16 +0600

    Tree Terminology

---
+++ 2.Graph Algorithms/2.Graph Traversel(DFS)/5.TreeTerminology.cpp
@@ -0,0 +1,20 @@
+Tree Terminology 
+
+1. All will be connected 
+2. No cycle 
+3. Number of Adages will be -1 from the node or vertex
+4. We have to fix a Root 
+
+some of key 
+* Child 
+* parent 
+* sibling 
+* leaf Root 
+* Root Node 
+* Ancestor 
+* Common Ancestor
+* lower common Ancestor
+
+
+***Depth : Upor thake nisay nambe root(0) ---> leaf
+***Height : Nich thake upore utbe leaf(0) ---> root
