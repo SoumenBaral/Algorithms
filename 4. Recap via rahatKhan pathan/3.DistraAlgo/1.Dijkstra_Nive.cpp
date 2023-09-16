56ca1747eb86b5eb44f8f74df4ce9c407165d36e
SoumenBaral <shumen.sb@gmail.com>
2023-09-16 15:05:51 +0600

    dijkstra

--- 4. Recap via rahatKhan pathan/3.DistraAlgo/1.Dijkstra_Nive.cpp
+++ 4. Recap via rahatKhan pathan/3.DistraAlgo/1.Dijkstra_Nive.cpp
@@ -1,11 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 const int N = 1e5 + 1;
-vector<pair<int,int>>adj[N];
+vector<pair<int,int>>v[N];
+int dis[N];
 void dijkstra(int s){
     queue<int>q;
     q.push(s);
-    q.pop();
+     dis[s] = 0;
+    while (!q.empty())
+    {
+        int parent = q.front();
+        q.pop();
+        for (int i = 0; i < v[parent].size(); i++)
+        {
+            pair<int, int> child = v[parent][i];
+            int childNode = child.first;
+            int childCost = child.second;
+            if (dis[parent] + childCost < dis[childNode])
+            {
+                dis[childNode] = dis[parent] + childCost;
+                q.push(childNode);
+            }
+        }
+    }  
 }
 
 int main ()
@@ -16,12 +33,18 @@ int main ()
     {
         int a,b,w;
         cin>>a>>b>>w;
-        adj[a].push_back({b,w});
-        adj[b].push_back({a,w});
+     v[a].push_back({b,w});
+     v[b].push_back({a,w});
 
     }
-    
+    for (int i = 1; i <= n; i++)
+    {
+        dis[i] = INT_MAX;
+    }
     dijkstra(1);
-
+     for (int i = 1; i <= n; i++)
+    {
+        cout << "Node " << i << ": " << dis[i] << endl;
+    }
     return 0;
 }
