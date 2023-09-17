50dc62e6156062ac94ce8a57d47abf6a8b4b15d9
SoumenBaral <shumen.sb@gmail.com>
2023-09-17 09:13:08 +0600

    path printing ;

---
+++ 4. Recap via rahatKhan pathan/3.DistraAlgo/3.2BfsPathByme.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 1e5+1;
+vector<int>adj[N];
+int dis[N];
+bool vis[N];
+int par[N];
+void bfs(int s){
+    queue<int>q;
+    q.push(s);
+    dis[s]=0;
+    par[s]=-1;
+    vis[s]=true;
+    while (!q.empty())
+    {
+        int parent = q.front();
+        q.pop();
+        for (int child :adj[parent])
+        {
+            if(!vis[child]){
+                q.push(child);
+                par[child]=parent;
+                dis[child] = dis[parent]+1;
+                vis[child] = true;
+            }
+        }
+        
+    }
+    
+}
+int main ()
+{
+    int n,e;
+    cin>>n>>e;
+    while(e--){
+        int a,b;
+        cin>>a>>b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+
+    }
+    bfs(1);
+    for (int i = 1; i <= n; i++)
+    {
+        cout << "Node " << i << ": " << par[i] << endl;
+    }
+    int d;
+    cin>>d;
+    if(vis[d]){
+        stack<int>path;
+        int x = d;
+        while (x!=-1)
+        {
+            // cout<<x<<endl;
+            path.push(x);
+            x = par[x];
+        }
+        while(!path.empty()){
+            int v = path.top();
+            path.pop();
+            cout<<v<<" ";
+        }
+        
+    }
+    else{
+        cout <<"Path nai"<<endl;
+    }
+    
+    return 0;
+}
